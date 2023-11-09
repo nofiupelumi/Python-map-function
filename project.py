08d8746f95e624add18b48e16cde5f010445df11
frontend project <nofiumoruf17@gmail.com>
2023-11-09 21:20:03 +0000

    Remove project.py

--- project.py
+++
@@ -1,8 +0,0 @@
-word= input('Enter a word: ')
-Word= word.lower()
-reversed_word= word[::-1]
-word = word.replace(' ','')
-if word==reversed_word:
-    print('The word is a paindrome.')
-else:
-    print('The word is not a palindrome.')
